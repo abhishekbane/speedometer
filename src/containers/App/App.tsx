0c98b7ceae4e84d0601d5ee91884fa4ac2659ae1
Abhishek Bane <abhishek.bane97@gmail.com>
2021-03-28 17:17:02 +0530

    Improved UI of App

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 import Speedometer from '../../components/Speedometer/Speedometer';
 
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.app}>
       <Speedometer low={0} high={220} value={0} showRandomSpeed/>
     </div>
   );
